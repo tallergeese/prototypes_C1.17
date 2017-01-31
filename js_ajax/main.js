aa59b838f76335891849e20a7cd94682a4dbd502
Michael Duong <Michael Duong>
2017-01-31 01:47:00 -0800

    js_ajax - Michael Duong

--- js_ajax/main.js
+++ js_ajax/main.js
@@ -1,5 +1,6 @@
 //Create GLOBAL variable below here on line 2
-
+    var global_result;
+    var first_movie;
 
 $(document).ready(function(){
     $('button').click(function(){
@@ -9,8 +10,21 @@ $(document).ready(function(){
             url: 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json',
             success: function(result) {
                 console.log('AJAX Success function called, with the following result:', result);
-
+                global_result = result;
+                first_movie = result.feed.entry[0]["im:image"][2].label;
+                for (var i = 0; i < result.feed.entry.length; i++){
+                    var movie_image = result.feed.entry[i]["im:image"][2].label;
+                    //the directors are in the title property for some reason too, but I'll keep both for completeness, although it's a bit redundant
+                    var movie_info = "Title: " + result.feed.entry[i].title.label + " Director: " + result.feed.entry[i]["im:artist"].label;
+                    var $fig= $('<figure>');
+                    var $figcap= $('<figcaption>').text(movie_info);
+                    var $img = $('<img>', {
+                        src: movie_image
+                    });
+                    $fig.append($img).append($figcap);
+                    $('#main').append($fig);
             }
+        }
         });
         console.log('End of click function');
     });
