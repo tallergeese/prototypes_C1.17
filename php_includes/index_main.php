e1df36db023e40369062ca010d99c1b7ace90623
Michael Duong <Michael Duong>
2017-02-15 17:54:54 -0800

    php_includes - Michael Duong

---
+++ php_includes/index_main.php
@@ -0,0 +1,7 @@
+<?php
+include('main.php');
+?>
+    <div>I'm not in the main content or the footer -- now with includes!</div>
+<?php
+include('footer.php');
+?>
---
+++ php_includes/main.php
@@ -0,0 +1,4 @@
+<main id="main_content">
+    Main Content
+</main>
+
