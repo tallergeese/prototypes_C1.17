f7a81e95660a7ed2c408da7b6cf429720d7c81af
Michael Duong <Michael Duong>
2017-02-18 18:05:44 -0800

    node webserver 0.9 - Michael Duong

--- node_webserver/webserver.js
+++ node_webserver/webserver.js
@@ -10,8 +10,8 @@ server.listen(port, function(){
 
 function requestHandler(request, response){
     console.log('handling request');
-    var directory = request.url.slice(1);
-    fs.readFile(directory, function(err, data){
+    var path = request.url.slice(1);
+    fs.readFile(path, function(err, data){
         if (err){
             fs.readFile('404.html', function(err,data){
                 response.writeHead(404);
@@ -19,8 +19,27 @@ function requestHandler(request, response){
             });
         }
         else{
+            response.setHeader('content-type', getMIME(path));
+            response.setHeader('emoji-type', ':tim:');
+            response.setHeader('secondary-emoji', ':little_g:');
             response.writeHead(200);
             response.end(data);
         }
     });
+}
+
+function getMIME(path){
+    var extensionIndex = path.lastIndexOf('.') + 1;
+    var extension = path.slice(extensionIndex);
+
+    switch(extension){
+        case 'html':
+            return "text/html";
+        case 'jpg':
+            return "image/jpeg";
+        case 'css':
+            return 'text/css';
+        default:
+            return 'not sure, buddy';
+    }
 }
