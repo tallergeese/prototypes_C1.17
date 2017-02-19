b455cf98d3c9a36893167cd9accb0484314e123e
Michael Duong <Michael Duong>
2017-02-18 17:00:26 -0800

    node webserver 0.5 - Michael Duong

--- node_webserver/webserver.js
+++ node_webserver/webserver.js
@@ -1,13 +1,24 @@
 var http = require('http');
+var fs = require('fs');
 var server = http.createServer(requestHandler);
+var port = 8888;
 
-server.listen(8888, function(){
+server.listen(port, function(){
     console.log('server listening');
 
 });
 
 function requestHandler(request, response){
-    console.log('handling request', request);
-    response.write('initializing request response\n');
-    response.end('It works!! Path Hit: ' + request.url);
+    console.log('handling request');
+    fs.readFile('index.html', function(err, data){
+        if (err){
+            response.writeHead(404);
+            response.end(JSON.stringify(err));
+            return;
+        }
+        else{
+            response.writeHead(200);
+            response.end(data);
+        }
+    });
 }
