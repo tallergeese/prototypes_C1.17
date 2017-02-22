d64ed92a8a76f311d1506b58163bd8b63c09b969
Michael Duong <Michael Duong>
2017-02-21 18:40:02 -0800

    feature set 1

--- mysql_basics/mysql_queries.sql
+++ mysql_basics/mysql_queries.sql
@@ -1,1 +1,8 @@
 Place your queries below
+SELECT * FROM `students` WHERE `username` = 'sgtdan'
+
+UPDATE `students` SET `email`='newemail@mememail.com' WHERE `username`='sgtdan'
+
+INSERT INTO `c117db`.`students` (`id`, `name`, `email`, `age`, `ssn`, `username`, `password`, `joined`, `status`) VALUES (NULL, 'Feature Set1', 'fs1@dfsdf.com', '23', '111555333', 'fs1', SHA1('hello'), NOW(), 'active');
+
+DELETE FROM `students` WHERE `username`='fs1'
