bb381dcf118819b0e4d8c89feeb69f2c2d286ccf
Michael Duong <Michael Duong>
2017-03-06 00:44:12 -0800

    ajax request coming back as an error, but has all the correct info

--- php_user_auth_basic/login_form.php
+++ php_user_auth_basic/login_form.php
@@ -1,1 +1,31 @@
-<!--login_form.php-->
+<!DOCTYPE html>
+<html>
+<head>
+  <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+  <script>
+    function sendLogin(){
+      $.ajax({
+        url: 'login_handler.php',
+        data: {"username": $('#username').val(), "password": $('#password').val()},
+        cache: "false",
+        method: "post",
+        dataType: "json",
+        success: function(res){
+          console.log(res);
+          $('input').val('');
+          $("#login-status").text(res.message);
+        },
+        error: function(err){
+          console.log('ajax error', err);
+        }
+      })
+    }
+  </script>
+</head>
+<body>
+  <div id="login-status"></div>
+  <input type="text" id="username" placeholder="username">
+  <input type="text" id="password" placeholder="password">
+  <button onclick="sendLogin()">Log In</button>
+</body>
+</html>
--- php_user_auth_basic/login_handler.php
+++ php_user_auth_basic/login_handler.php
@@ -1,1 +1,35 @@
 <!--login_handler.php-->
+<?php
+$user_info = [
+    ['id'=> 0, 'username'=>'dpaschal', 'password' => '2beb0192eb1ca5a8756bc89a09b93036e1854049'],
+    ['id'=> 1, 'username'=>'rickybobby', 'password' => '75510278cc01da664793802ac6fd1e0c9c14d08d'],
+    ['id'=> 2, 'username'=>'darkhelmet', 'password' => '8cb2237d0679ca88db6464eac60da9634551396412345'],
+    ['id'=> 3, 'username'=>'heydude', 'password'=> '988d52642fa0dc4e7c0ad7803adcb6e2a5914d85'],
+    ['id'=> 4, 'username'=>'whoahdude', 'password'=> 'f35eb53d904a7c4a21618041502798fd42692eaf'],
+];
+
+$output = ['success'=> null, 'user_id'=> null, 'message' => null];
+
+$user['username'] = $_POST['username'];
+$user['password'] = sha1($_POST['password']);
+
+for ($i = 0; $i < sizeof($user_info); $i++){
+  if ($user['username'] === $user_info[$i]['username']){
+    if ($user['password'] === $user_info[$i]['password']){
+      $output['user_id'] = $user_info[$i]['id']; 
+    }
+  }
+}
+
+if (isset($output["user_id"])){
+  $output["success"] = "true";
+  $output["message"] = "Login Success";
+}
+else{
+  $output["success"] = "false";
+  $output["status"] = "Login Failed";
+}
+$output_string = json_encode($output);
+header('Content-Type: application/json');
+echo($output_string);
+?>
